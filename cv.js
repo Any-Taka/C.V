e5ed5cd6af15935e9e949ababe3cbaca52c1329b
Any-Taka <123767574+Any-Taka@users.noreply.github.com>
2023-01-27 16:02:45 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,6 @@
+
+console.log("Hola, si estas viendo esto probablemente seas un o una programador, asi que quiero decir que esta es mi pagina web... Las contaseñas son ''lionel''")
+var Numero1 = 10;
+var Numero2 = 20;
+var resultado = Numero1+Numero2;
+var Numero1, Numero2 = 30;
